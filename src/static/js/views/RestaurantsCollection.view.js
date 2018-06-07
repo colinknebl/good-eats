03c5803f3c8526360be219967451a24b61f78f31
Colin Knebl <Colin.Knebl@2020spaces.com>
2018-06-07 10:28:15 -0400

    initial commit: config complete

---
+++ src/static/js/views/RestaurantsCollection.view.js
@@ -0,0 +1,13 @@
+define('RestaurantsCollectionView', [
+  /* 1 */ 'jquery', 
+  /* 2 */ 'underscore', 
+  /* 3 */ 'backbone'
+], function(
+  /* 1 */ $, 
+  /* 2 */ _, 
+  /* 3 */ Backbone) 
+{
+
+
+
+})
